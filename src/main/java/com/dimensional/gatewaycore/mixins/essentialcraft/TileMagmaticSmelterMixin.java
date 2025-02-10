1d419453a68b3dc2d57a08129af1b243b819a0f9
Wizzerinus <quillinde@gmail.com>
2025-02-10 15:43:15 +0300

    ec4: account for remap bug in MixinBooter

--- src/main/java/com/dimensional/gatewaycore/mixins/essentialcraft/TileMagmaticSmelterMixin.java
+++ src/main/java/com/dimensional/gatewaycore/mixins/essentialcraft/TileMagmaticSmelterMixin.java
@@ -1,39 +1,21 @@
 package com.dimensional.gatewaycore.mixins.essentialcraft;
 
 import com.dimensional.gatewaycore.GatewayConfig;
-import com.llamalad7.mixinextras.expression.Definition;
-import com.llamalad7.mixinextras.expression.Expression;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.sugar.Local;
 import essentialcraft.common.tile.TileMagmaticSmelter;
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.fluids.FluidStack;
-import net.minecraftforge.fluids.FluidTank;
-import net.minecraftforge.fluids.FluidUtil;
+import net.minecraftforge.fluids.*;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(value = TileMagmaticSmelter.class, remap = false)
 public class TileMagmaticSmelterMixin {
-    @Shadow public FluidTank lavaTank;
 
-    @Definition(id = "getFluid", method = "Lnet/minecraftforge/fluids/FluidStack;getFluid()Lnet/minecraftforge/fluids/Fluid;", remap = false)
-    @Definition(id = "LAVA", field = "Lnet/minecraftforge/fluids/FluidRegistry;LAVA:Lnet/minecraftforge/fluids/Fluid;", remap = false)
-    @Expression("?.getFluid() == LAVA")
-    @ModifyExpressionValue(method = "update", at = @At("MIXINEXTRAS:EXPRESSION"), remap = true)
-    private boolean changeFluid(boolean original) {
-        if (lavaTank.getFluid() == null) return false;
-        return lavaTank.getFluid().getFluid().getName().equals(GatewayConfig.mods.magmaticSmelteryFluid);
+    @SuppressWarnings({"UnresolvedMixinReference", "MixinAnnotationTarget"})
+    @ModifyExpressionValue(method = "*", at = @At(value = "FIELD", opcode = Opcodes.GETSTATIC,
+            target = "Lnet/minecraftforge/fluids/FluidRegistry;LAVA:Lnet/minecraftforge/fluids/Fluid;", remap = false))
+    private Fluid changeFluid(Fluid original) {
+        return FluidRegistry.getFluid(GatewayConfig.mods.magmaticSmelteryFluid);
     }
 
-    @Definition(id = "getFluid", method = "Lnet/minecraftforge/fluids/FluidStack;getFluid()Lnet/minecraftforge/fluids/Fluid;", remap = false)
-    @Definition(id = "LAVA", field = "Lnet/minecraftforge/fluids/FluidRegistry;LAVA:Lnet/minecraftforge/fluids/Fluid;", remap = false)
-    @Expression("?.getFluid() == LAVA")
-    @ModifyExpressionValue(method = "isItemValidForSlot", at = @At("MIXINEXTRAS:EXPRESSION"), remap = false)
-    private boolean isFluidValid(boolean original, @Local(argsOnly = true) ItemStack stack) {
-        FluidStack fluid = FluidUtil.getFluidContained(stack);
-        if (fluid == null) return false;
-        return fluid.getFluid().getName().equals(GatewayConfig.mods.magmaticSmelteryFluid);
-    }
 }
